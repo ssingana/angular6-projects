0a5263a3ccd4a5c9d749ea5f6df45c8d0ec4e127
ssingana <ssingana@gmail.com>
2018-10-26 22:31:30 +0530

    firebase jwt auth sample.

---
+++ authorization_sample/src/app/testcom/testcom.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testcom',
+  templateUrl: './testcom.component.html',
+  styleUrls: ['./testcom.component.css']
+})
+export class TestcomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  myname = 'sreeni';
+
+  department ;
+
+}
